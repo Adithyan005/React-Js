eb1a41d1354e28bacb00534710f3e6d5655eed51
Adithyan <adi6239100@gmail.com>
2024-08-06 23:37:20 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <h1>hai</h1>
-    </div>
-  )
+    this.state = {
+      name: "",
+      mail: "",
+    };
+    this.handle1 = this.handle1.bind(this);
+    this.handle2 = this.handle2.bind(this);
+    this.handlesubmit = this.handlesubmit.bind(this);
+  }
+  handle1(event) {
+    this.setState({ name: event.target.value });
+  }
+  handle2(event) {
+    this.setState({ mail: event.target.value });
+  }
+  handlesubmit(event) {
+    alert("Name: " + this.state.name + "\nEmail: " + this.state.mail);
+  }
+
+  render() {
+    return (
+      <>
+        <h1 className="text-center">Forms</h1>
+        <form action="">
+          <div className="row md-2">
+            <div className="col md-6 m-3 p-1">
+              <label htmlFor="">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                value={this.state.name}
+                onChange={this.handle1}
+              />
+              <br />
+            </div>
+
+            <div className="col md-6 m-3 p-1">
+              <label htmlFor="">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                value={this.state.mail}
+                onChange={this.handle2}
+              />
+              <br />
+            </div>
+          </div>
+
+          <div className="mb-2 p-2 mx-auto">
+            <button
+              onClick={this.handlesubmit}
+              className="btn btn-success text-center"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </>
+    );
+  }
 }
-
-export default App
+export default App;
